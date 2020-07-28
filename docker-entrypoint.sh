363f539e1db7b86597a13ffdc211ff07b31f9fa5
Jeremie <jeremie.chaloin@gmail.com>
2020-07-28 10:51:46 +0200

    updTmpl

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 # Default command (assuming container start)
 if [ "$1" = 'supervisord' ]; then
+	
 	# $DNS_DOMAIN IP config for dnsmasq
 	domain_conf="/etc/dnsmasq.d/${DNS_DOMAIN}.conf"
 	echo "Generating configuration in $domain_conf"
@@ -10,6 +11,7 @@ if [ "$1" = 'supervisord' ]; then
 	echo "address=/${DNS_DOMAIN}/${DNS_IP}" | tee -a $domain_conf
 	# Reverse resolution of $DNS_IP to ${DNS_DOMAIN}
 	echo $DNS_IP | awk -v dzone=${DNS_DOMAIN} -F . '{print "ptr-record="$4"."$3"."$2"."$1".in-addr.arpa,"dzone}' | tee -a $domain_conf
+	
 	# Turn query loggin on
 	if [ "$LOG_QUERIES" = true ]; then
 		sed -i '/log-queries/ s/^#*//' /etc/dnsmasq.conf
@@ -17,7 +19,7 @@ if [ "$1" = 'supervisord' ]; then
 		sed -i '/log-queries/ s/^#*/#/' /etc/dnsmasq.conf
 	fi
 	# Start supervisord
-	exec /usr/bin/supervisord -n
+	exec /usr/bin/supervisord -n -u root -c /etc/supervisord.conf
 fi
 # Any other command (assuming container already running)
 exec "$@"
